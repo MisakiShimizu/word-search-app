983358fe2b7663617da09a7b51495fd875cf43ae
Misaki <tshimizumisaki@gmail.com>
2022-01-15 12:18:21 -0500

    added axios and useEffect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  // const apiKey = "ecee6254-f8f9-42d1-b310-d45f930f7e04";
+  const apiKey = "ecee6254-f8f9-42d1-b310-d45f930f7e04";
+
+  const [word, setWord] = useState([]);
+  const [userInput, setUserInput] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
+
+  // useEffect to mkae API to Merriam Webster API
+  useEffect( () => {
+    axios({
+      url: `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${searchTerm}`,
+      method: "GET",
+      dataResponse: "json",
+      params: {
+        key: apiKey,
+      }
+    }).then( (response) => {
+      console.log(response.data);
+      setWord(response.data);
+    });
+  }, [searchTerm]);
   return (
     <div className="App">
       <header>

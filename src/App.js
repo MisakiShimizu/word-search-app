1fb73c988831d76c46fe65f8eacd552e4a8bcd78
Misaki <tshimizumisaki@gmail.com>
2022-01-15 11:38:25 -0500

    Added fontawesome to react :rocket:

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // const apiKey = "ecee6254-f8f9-42d1-b310-d45f930f7e04";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Word Search</h1>
       </header>
+      <form>
+        <label htmlFor="search">Look through the dictionary here</label>
+        <input type="text" id="search"/>
+        <button></button>
+      </form>
+
+      
     </div>
   );
 }
